eb68d0a319d3bc6bb2a0a454b637eb29a3361ff6
Hassan Alkhalaf <baraanas1521@gmail.com>
2026-05-28 00:26:14 +0300

    new loading

--- src/components/SearchUser.tsx
+++ src/components/SearchUser.tsx
@@ -19,6 +19,7 @@ const SearchUser = ({ onUserSelect }: NavProps) => {
           if (key === "Enter") {
             onUserSelect?.(searchTerm);
             setSearchTerm("");
+            
           }
         }}
       />
