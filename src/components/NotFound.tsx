2ebd3b19b3343851cbf97b7d253041e296250cb4
Hassan Alkhalaf <baraanas1521@gmail.com>
2026-06-01 20:05:39 +0300

    Not Found page add new icon

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Loading from "./Loading";
-
+import imgNotfound from "../assets/web.png"
 interface Props {
   error?: string;
 
@@ -30,6 +30,7 @@ const NotFound = (props: Props) => {
     <>
       {hasError && (
         <div className={`error ${hasError ? "active" : ""}`}>
+          <img src={imgNotfound} alt="Not Found Page 404" />
           <p>{props.error}</p>
 
           <button type="button" onClick={handleClickHomePage}>
